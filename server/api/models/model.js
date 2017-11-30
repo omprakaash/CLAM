3214e9f95a35871e0b76f87f0c18056ec03c7a2e
eminnguyen <emilyhuongnguyen@gmail.com>
2017-11-29 19:26:03 -0800

    Add base student, room, and database

--- server/api/models/model.js
+++ server/api/models/model.js
@@ -3,3 +3,33 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // TODO: create database model
+
+//Students have ID, SeatNumber, Name
+var Student = new Schema({
+   name: {
+     type:String,
+     required: true
+   }
+   studentID: {
+     type: Number,
+   }
+   seatNumber: {
+     type: Number,
+   }
+})
+
+//Each room contains a list of students
+var Room = new Schema({
+  roomNumber: {
+    type: Number,
+  }
+  students: [Student]
+})
+
+//Database contains list of rooms and students
+var Database = new Schema({
+  rooms: [Room]
+  students: [Student]
+})
+
+module.exports = mongoose.model('Database', Database);
